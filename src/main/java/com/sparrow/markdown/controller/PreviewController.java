39e929791bed206bfd6e1571998cf4b13853a391
harry <zh_harry@163.com>
2019-06-28 09:10:57 +0800

    Default Changelist

--- src/main/java/com/sparrow/markdown/controller/PreviewController.java
+++ src/main/java/com/sparrow/markdown/controller/PreviewController.java
@@ -5,10 +5,14 @@ import com.sparrow.markdown.parser.MarkParser;
 import com.sparrow.markdown.parser.impl.MarkdownParserComposite;
 import com.sparrow.markdown.vo.MarkdownVO;
 import com.sparrow.mvc.RequestParameters;
+import com.sparrow.utility.StringUtility;
 
 public class PreviewController {
     @RequestParameters("markdown")
     public MarkdownVO preview(String markdown){
+        if(StringUtility.isNullOrEmpty(markdown)){
+            return new MarkdownVO("");
+        }
         MarkContext markContext = new MarkContext(markdown);
         MarkParser markParser = MarkdownParserComposite.getInstance();
         markParser.parse(markContext);
