f23fc8e40053f1df07858cfd0fd98806eda0660c
harry <zhanglz@missfresh.cn>
2022-05-23 09:47:42 +0800

    commit

--- source/scripts/sparrow.js
+++ source/scripts/sparrow.js
@@ -495,3 +495,58 @@ Sparrow.prototype.enter = function (handle) {
         }
     };
 };
+
+
+String.prototype.toArrayBuffer=function(){
+  var bytes = [];
+  var len, c;
+  len = this.length;
+  for (var i = 0; i < len; i++) {
+    c = this.charCodeAt(i);
+    if (c >= 0x010000 && c <= 0x10FFFF) {
+      bytes.push(((c >> 18) & 0x07) | 0xF0);
+      bytes.push(((c >> 12) & 0x3F) | 0x80);
+      bytes.push(((c >> 6) & 0x3F) | 0x80);
+      bytes.push((c & 0x3F) | 0x80);
+    } else if (c >= 0x000800 && c <= 0x00FFFF) {
+      bytes.push(((c >> 12) & 0x0F) | 0xE0);
+      bytes.push(((c >> 6) & 0x3F) | 0x80);
+      bytes.push((c & 0x3F) | 0x80);
+    } else if (c >= 0x000080 && c <= 0x0007FF) {
+      bytes.push(((c >> 6) & 0x1F) | 0xC0);
+      bytes.push((c & 0x3F) | 0x80);
+    } else {
+      bytes.push(c & 0xFF);
+    }
+  }
+  var array = new Int8Array(bytes.length);
+  for (var i in bytes) {
+    array[i] = bytes[i];
+  }
+  return array;
+};
+
+Int8Array.prototype.toString=function() {
+  var dataView = new DataView(this.buffer);
+  var ints = new Uint8Array(this.buffer.byteLength);
+  for (var i = 0; i < ints.length; i++) {
+    ints[i] = dataView.getUint8(i);
+  }
+  var str = '', _arr = ints;
+  for (var i = 0; i < _arr.length; i++) {
+    var one = _arr[i].toString(2),
+        v = one.match(/^1+?(?=0)/);
+    if (v && one.length == 8) {
+      var bytesLength = v[0].length;
+      var store = _arr[i].toString(2).slice(7 - bytesLength);
+      for (var st = 1; st < bytesLength; st++) {
+        store += _arr[st + i].toString(2).slice(2);
+      }
+      str += String.fromCharCode(parseInt(store, 2));
+      i += bytesLength - 1;
+    } else {
+      str += String.fromCharCode(_arr[i]);
+    }
+  }
+  return str;
+};
