2a7624b4347becdd4be984e6fe2106973290e0d8
harry <zh_harry@163.com>
2023-05-11 23:41:52 +0800

    add websocket add chat

--- vue/chat-pc/src/components/chartItem.vue
+++ vue/chat-pc/src/components/chartItem.vue
@@ -1,6 +1,5 @@
 <template>
   <div :class="messageItem.isMe ? 'right' : 'left'" class="message">
-    <el-table @row-contextmenu="rightClick"></el-table>
     <!-- 根据条件展示聊天时间 -->
     <div v-if="messageItem.time" class="time">
       {{ messageItem.time }}
@@ -9,27 +8,20 @@
       <img :src="messageItem.avatar" alt="" class="avatar-user" />
       <!-- 文本信息和图片信息 动态显示 -->
 
-      <div v-if="messageItem.isText" class="message-detail">
+      <div
+        v-if="messageItem.isText"
+        class="message-detail"
+        v-on:contextmenu="rightClick(messageItem, $event)"
+      >
         <div class="user-name">{{ messageItem.userName }}</div>
         <span class="block"></span>
         <span class="message-text">{{ messageItem.content }}</span>
       </div>
       <!-- 图片类型 -->
-      <div v-else class="msg-picture-detail">
+      <div v-else class="msg-picture-detail" v-on:contextmenu="rightClick">
         <div class="user-name">{{ messageItem.userName }}</div>
         <img :src="messageItem.imgUrl" alt="" class="msg-picture" />
       </div>
-    </div>
-    <div id="menu" class="menuDiv">
-      <ul class="menuUl">
-        <li
-          v-for="(item, index) in menus"
-          :key="index"
-          @click.stop="alert(index)"
-        >
-          <i :class="item.icon"></i> {{ item.name }}
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -44,67 +36,28 @@ export default {
   },
   methods: {
     // 表格右击的功能
-    rightClick(row, column, event) {
-      this.$alert("你点击了" + row.name + "的" + column.label);
-      event.preventDefault();
-      let menu = document.querySelector("#menu");
+    rightClick(item, e) {
+      console.log(item);
+      // 阻止默认的菜单弹出事件
+      e.preventDefault();
+      let menu = document.querySelector(".msg-recall");
       // 根据事件对象中鼠标点击的位置，进行定位
-      menu.style.left = event.clientX - 258 + "px";
-      menu.style.top = event.clientY - 75 + "px";
-      // 改变自定义菜单的隐藏与显示
       menu.style.display = "block";
+      menu.style.top = e.pageX - 800 + "px";
+      menu.style.left = e.pageY + 50 + "px";
       menu.style.zIndex = 1000;
     },
   },
   data() {
     return {
       //右键菜单栏
-      menus: [
-        { name: "菜单一", operType: 1, icon: "el-icon-upload2" },
-        { name: "菜单二", operType: 2, icon: "el-icon-folder-add" },
-        { name: "菜单三", operType: 3, icon: "el-icon-edit-outline" },
-        { name: "菜单四", operType: 4, icon: "el-icon-folder-remove" },
-        { name: "菜单五", operType: 5, icon: "el-icon-download" },
-      ],
+      menus: [{ name: "菜单四", operType: 4, icon: "el-icon-folder-remove" }],
     };
   },
 };
 </script>
 
 <style lang="less" scoped>
-// 菜单样式
-.menuDiv {
-  position: absolute;
-
-  .menuUl {
-    height: auto;
-    width: auto;
-    font-size: 14px;
-    text-align: left;
-    border-radius: 4px;
-    border: none;
-    background-color: #ffffff;
-    color: #606266;
-    list-style: none;
-    border: 1px solid #ebeef5;
-
-    li {
-      width: 140px;
-      height: 35px;
-      line-height: 35px;
-      padding: 0 10px;
-      cursor: pointer;
-      border-bottom: 1px solid rgba(255, 255, 255, 0.47);
-
-      &:hover {
-        display: block;
-        background-color: #ecf5ff;
-        color: #7abbff;
-      }
-    }
-  }
-}
-
 .message {
   margin: 20px;
 
