4b576a7f0d08f1559c23166b5a4b6175dc3c3942
harry <zh_harry@163.com>
2023-05-11 23:41:52 +0800

    add websocket add chat

--- vue/chat-pc/src/components/chatPart.vue
+++ vue/chat-pc/src/components/chatPart.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="">
+    <div class="msg-recall" style="display: none">撤回</div>
     <div class="msg-content">
       <!-- 聊天头部信息 -->
       <div class="title">
@@ -153,6 +154,30 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.msg-recall {
+  position: absolute;
+  //left: 0;
+  //top: 0;
+  //color: red;
+  //background-color: #fff;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.msg-recall ul {
+  //list-style-type: none;
+  //padding: 0;
+  //margin: 0;
+}
+
+.msg-recall ul li {
+  //padding: 5px 15px;
+  //cursor: pointer;
+}
+
+.msg-recall ul li:hover {
+  //background: rgba(0, 0, 0, 0.1);
+}
+
 .msg-content {
   // float: right;
   width: 100%;
