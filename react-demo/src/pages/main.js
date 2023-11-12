521187f55c76e69a78293a90819f72014ae9227d
zh_harry <zh_harry@163.com>
2023-11-12 23:41:07 +0800

    init

--- react-demo/src/pages/main.js
+++ react-demo/src/pages/main.js
@@ -1,13 +1,16 @@
 import {greet} from "./layout/heading";
 import './main.css'
-
+greet("zhagnsan");
 /**
  * 一定有dom 使用的方法才会被导出，否则不预导出
  */
-document.getElementById("hello").addEventListener('click', function () {
-    alert(greet('zhangsan'));
-    alert(greet);
-});
+// document.getElementById("hello").addEventListener('click', function () {
+//     alert(greet('zhangsan'));
+//     alert(greet);
+// });
 
-document.getElementById("hello").classList.add("main");
+export default  function (){
+    greet("zhangsan");
+}
+// document.getElementById("hello").classList.add("main");
 
--- react-demo/webpack.config.js
+++ react-demo/webpack.config.js
@@ -3,19 +3,29 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const MiddlewarePlugin = require("next/dist/build/webpack/plugins/middleware-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+    optimization: {
+        // usedExports: false, // 禁用仅导出使用的导出
+        sideEffects: false, // 禁用副作用检测（对于 package.json 中的 "sideEffects" 属性）
+        minimize: false, // <---- 禁用 uglify.
+    },
     mode: "development", //开发模式
     entry: {
+        // main_css:path.resolve(__dirname,"src","pages","main.css"),
         index: path.resolve(__dirname, "src", "index.js"),
         main: path.resolve(__dirname, "src", "pages", "main.js"),
-
     }, //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
         path: path.resolve(__dirname, "public"), // 输出的路径
-        filename: "[name]-bundle.js", // 打包后文件
+        filename: "[name]-wrap.js", // 打包后文件
     },
     module: {
         rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
             {
                 test: /\.(js|jsx)$/,
                 loader: "babel-loader",
@@ -27,17 +37,22 @@ module.exports = {
                     'html-withimg-loader'
                 ]
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',//后执行
-                    'css-loader',//先执行
-                ]
-            }
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',//后执行
+            //         'css-loader',//先执行
+            //     ]
+            // }
         ],
     },
 
     plugins: [
+        // 提取css成单独文件
+        new MiniCssExtractPlugin({
+            // 定义输出文件名和目录
+            filename: "css/[name]-wrap-[hash].css",
+        }),
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             title: "Webpack App",
