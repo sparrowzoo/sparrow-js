c07c36aab200d8c5343a43dd2a4cac6a47518371
zh_harry <zh_harry@163.com>
2023-11-13 00:54:31 +0800

    init

--- react-demo/src/pages/main.js
+++ react-demo/src/pages/main.js
@@ -1,5 +1,6 @@
 import {greet} from "./layout/heading";
 import './main.css'
+import logo from './logo.png'
 greet("zhagnsan");
 /**
  * 一定有dom 使用的方法才会被导出，否则不预导出
@@ -13,4 +14,8 @@ export default  function (){
     greet("zhangsan");
 }
 // document.getElementById("hello").classList.add("main");
+
+const  image=new Image();
+image.src=logo;
+document.body.append(image);
 
