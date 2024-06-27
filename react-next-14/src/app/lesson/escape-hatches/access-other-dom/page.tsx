7924888a880eb280c00c206457687f50d4049e00
张立志 <zhanglizhi@tedu.cn>
2024-06-27 12:21:19 +0800

    add react demo

--- react-next-14/src/app/lesson/escape-hatches/access-other-dom/page.tsx
+++ react-next-14/src/app/lesson/escape-hatches/access-other-dom/page.tsx
@@ -1,38 +1,36 @@
-'use client'
-import {forwardRef, useRef} from 'react';
+"use client";
+import { forwardRef, useRef } from "react";
 
-const ForwardInput = forwardRef((props, ref:any) => {
-    return <input {...props} ref={ref} />;
+const ForwardInput = forwardRef((props, ref: any) => {
+  return <input {...props} ref={ref} />;
 });
+ForwardInput.displayName = "自定义Name";
 
 function MyInput(props: any) {
-    return <input {...props} />;
+  return <input {...props} />;
 }
 
-export default function MyForm() {
-    const inputRef = useRef(null);
+export default function Page() {
+  const inputRef = useRef(null);
 
-    function handleClick() {
-        if (inputRef.current) {
-            // @ts-ignore
-            inputRef.current.focus();
-        }
+  function handleClick() {
+    if (inputRef.current) {
+      // @ts-ignore
+      inputRef.current.focus();
     }
+  }
 
-    return (
-        <>
-            <p>
-                Warning: Function components cannot be given refs. Attempts to access this ref will fail. Did you mean
-                to use React.forwardRef()?
-            </p>
-            <MyInput ref={inputRef}/>
-
-            <ForwardInput ref={inputRef}/>
-            <br/>
-            {/*<input ref={inputRef}/>*/}
-            <button onClick={handleClick}>
-                聚焦输入框
-            </button>
-        </>
-    );
+  return (
+    <>
+      <p>
+        Warning: Function components cannot be given refs. Attempts to access
+        this ref will fail. Did you mean to use React.forwardRef()?
+      </p>
+      <MyInput ref={inputRef} />
+      <ForwardInput ref={inputRef} />
+      <br />
+      <input ref={inputRef} />
+      <button onClick={handleClick}>聚焦输入框</button>
+    </>
+  );
 }
