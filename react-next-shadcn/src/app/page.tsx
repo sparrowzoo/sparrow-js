5d1b0f22d7b772ac49074f109065767f97bd0f20
zh_harry <zh_harry@163.com>
2024-07-05 02:21:16 +0800

    modify webpack  配置及说明文档

---
+++ react-next-shadcn/src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (<>
+            <div className="bg-background text-foreground">To use CSS variables for theming set tailwind.cssVariables
+                to true in your components.json file.
+                className="bg-background text-foreground"
+
+            </div>
+            <div className="bg-zinc-950 dark:bg-white">Utility classes
+                 className="bg-zinc-950 dark:bg-white"     "cssVariables": false
+            </div>
+
+            <div className="bg-primary text-primary-foreground">Hello</div>
+
+        </>
+    );
+}
