1955f274593c43349fe65d40ed84431fb46acdc7
张立志 <zhanglizhi@tedu.cn>
2024-07-06 14:14:46 +0800

    add react demo

--- react-next-shadcn/postcss.config.mjs
+++ react-next-shadcn/postcss.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': {},//默认是postcss-nested 更好
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
 
 export default config;
