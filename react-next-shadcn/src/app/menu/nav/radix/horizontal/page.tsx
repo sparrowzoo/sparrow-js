81443cc76c25ecff8735a3a4423d29a8c79f6b5e
zh_harry <zh_harry@163.com>
2024-07-18 01:14:45 +0800

    布局

---
+++ react-next-shadcn/src/app/menu/nav/radix/horizontal/page.tsx
@@ -0,0 +1,176 @@
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import React from "react";
+import {cn} from "@/lib/utils";
+
+const ListItem = React.forwardRef(
+    (
+        {
+            className,
+            children,
+            title,
+            ...props
+        }: {
+            className?: string;
+            children: React.ReactNode;
+            title: string;
+            props: any;
+        },
+        forwardedRef
+    ) => (
+        <li>
+            <NavigationMenu.Link asChild>
+                <a
+                    className={cn(
+                        "focus:shadow-[0_0_0_2px] focus:shadow-violet7 hover:bg-mauve3 block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-violet12 mb-[5px] font-medium leading-[1.2]">
+                        {title}
+                    </div>
+                    <p className="text-mauve11 leading-[1.4]">{children}</p>
+                </a>
+            </NavigationMenu.Link>
+        </li>
+    )
+);
+ListItem.displayName = "ListItem";
+export default function Page() {
+    return (
+        <div>
+            <h1>Horizontal Nav</h1>
+            <NavigationMenu.Root orientation="horizontal"
+                                 className="relative z-[1] bg-black flex sm:w-screen w-[200px] justify-center">
+                <NavigationMenu.List
+                    className="center shadow-blackA4 m-0 flex sm:flex-row flex-col  list-none rounded-[6px] text-white bg-block p-1 shadow-[0_2px_10px]">
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Trigger
+                            className="text-violet11 bg-block hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+                            Learn{" "}
+                        </NavigationMenu.Trigger>
+                        <NavigationMenu.Content
+                            className="data-[motion=from-start]:animate-enterFromLeft bg-block data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto">
+                            <ul className="one m-0 grid bg-block list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
+                                <li className="row-span-3 grid">
+                                    <NavigationMenu.Link asChild>
+                                        <a
+                                            className="focus:shadow-violet7 from-purple9 to-indigo9 flex
+                    h-full w-full select-none flex-col justify-end rounded-[6px] bg-gradient-to-b p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px]"
+                                            href="/"
+                                        >
+                                            <svg
+                                                aria-hidden
+                                                width="38"
+                                                height="38"
+                                                viewBox="0 0 25 25"
+                                                fill="white"
+                                            >
+                                                <path
+                                                    d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
+                                                <path d="M12 0H4V8H12V0Z"></path>
+                                                <path
+                                                    d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
+                                            </svg>
+                                            <div
+                                                className="bg-block mt-4 mb-[7px] text-[18px] font-medium leading-[1.2] text-white">
+                                                Radix Primitives
+                                            </div>
+                                            <p className="text-mauve4 text-[14px] leading-[1.3]">
+                                                Unstyled, accessible components for React.
+                                            </p>
+                                        </a>
+                                    </NavigationMenu.Link>
+                                </li>
+
+                                <ListItem title="Stitches" props={{href: "/stitches"}}>
+                                    CSS-in-JS with best-in-class developer experience.
+                                </ListItem>
+                                <ListItem title="Colors" props={{href: "/colors"}}>
+                                    Beautiful, thought-out palettes with auto dark mode.
+                                </ListItem>
+                                <ListItem
+                                    props={{
+                                        target: "_blank",
+                                        href: "https://radix-ui.com/primitives/docs/components/icons",
+                                    }}
+                                    title="Icons"
+                                >
+                                    A crisp set of 15x15 icons, balanced and consistent.
+                                </ListItem>
+                            </ul>
+                        </NavigationMenu.Content>
+                    </NavigationMenu.Item>
+
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Trigger
+                            className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+                            Overview{" "}
+                        </NavigationMenu.Trigger>
+                        <NavigationMenu.Content className="bg-block absolute top-0 left-0 w-full sm:w-auto">
+                            <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3">
+                                <ListItem
+                                    title="Introduction"
+                                    props={{href: "/primitives/docs/overview/introduction"}}
+                                >
+                                    Build high-quality, accessible design systems and web apps.
+                                </ListItem>
+                                <ListItem
+                                    props={{href: "/primitives/docs/overview/installation"}}
+                                    title="Getting started"
+                                >
+                                    A quick tutorial to get you up and running with Radix
+                                    Primitives.
+                                </ListItem>
+                                <ListItem
+                                    title="Styling"
+                                    props={{href: "/primitives/docs/overview/styling"}}
+                                >
+                                    Unstyled and compatible with any styling solution.
+                                </ListItem>
+                                <ListItem
+                                    title="Animation"
+                                    props={{href: "/primitives/docs/overview/animation"}}
+                                >
+                                    Use CSS keyframes or any animation library of your choice.
+                                </ListItem>
+                                <ListItem
+                                    title="Accessibility"
+                                    props={{href: "/primitives/docs/overview/accessibility"}}
+                                >
+                                    Tested in a range of browsers and assistive technologies.
+                                </ListItem>
+                                <ListItem
+                                    title="Releases"
+                                    props={{href: "/primitives/docs/overview/releases"}}
+                                >
+                                    Radix Primitives releases and their changelogs.
+                                </ListItem>
+                            </ul>
+                        </NavigationMenu.Content>
+                    </NavigationMenu.Item>
+
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link
+                            className="text-violet11  hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                            href="https://github.com/radix-ui"
+                        >
+                            Github
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+
+                    <NavigationMenu.Indicator
+                        className="data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]">
+                        <div className="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-block"/>
+                    </NavigationMenu.Indicator>
+                </NavigationMenu.List>
+
+                <div className="perspective-[2000px] absolute sm:top-full sm:left-0 top-0 left-full flex w-full justify-center">
+                    <NavigationMenu.Viewport
+                        className="data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-block transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]"/>
+                </div>
+            </NavigationMenu.Root>
+
+        </div>
+    )
+}
