543468e5bceabcdfcaab53b49f93b9ea2b65a79b
zh_harry <zh_harry@163.com>
2024-07-22 00:33:11 +0800

    布局

---
+++ react-webpack/src/npm-component-publish/dist/index.d.ts
@@ -0,0 +1,11 @@
+export interface StudentProps {
+    name: string;
+    class: string;
+}
+declare class Student {
+    name: string;
+    class: string;
+    constructor(std: StudentProps);
+}
+export default Student;
+//# sourceMappingURL=index.d.ts.map
