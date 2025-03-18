2da5943035cd0c738490ddf1951c169ff529cd82
张立志 <zhanglizhi@tedu.cn>
2025-03-18 18:32:46 +0800

    add react demo

---
+++ react-next-14/src/components/Login.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useState } from "react";
+import { Fetcher } from "@/lib/Fetcher";
+
+export default function Login() {
+  const [userName, setUserName] = useState("");
+
+  function handleLogin() {
+    console.log("login", userName);
+    Fetcher.post(
+      "/chat/v2/login",
+      JSON.stringify({
+        id: userName,
+        category: 1,
+      })//必须序列化
+    );
+  }
+
+  function longHandleLogin() {
+    console.log("login", userName);
+    Fetcher.post("/chat/v2/long-login", userName);//必须是字符串
+    //Fetcher.post("/chat/v2/long-login", parseInt(userName,10));//数字不允许
+    //Fetcher.post("/chat/v2/long-login", JSON.stringify(userName));//对象不允许
+  }
+
+  return (
+    <div>
+      <h2>登录</h2>
+      <div>
+        <input
+          className={"text-red-700"}
+          onChange={(e) => setUserName(e.target.value)}
+          type="text"
+          placeholder="用户名"
+        />
+        <button
+          onClick={() => {
+            console.log(userName);
+            handleLogin();
+          }}
+        >
+          登录
+        </button>
+        <button
+          onClick={() => {
+            console.log(userName);
+            longHandleLogin();
+          }}
+        >
+          Long UserId登录
+        </button>
+      </div>
+    </div>
+  );
+}
--- react-next-14/src/lib/Fetcher.ts
+++ react-next-14/src/lib/Fetcher.ts
@@ -1,37 +1,71 @@
 import getToken from "@/lib/TokenUtils";
 import { API_BASIC_URL } from "@/lib/EnvUtils";
+import toast from "react-hot-toast";
 
 //https://nextjs.org/docs/app/getting-started/fetching-data
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Window/fetch
+class Fetcher {
+  static async get(url, withToken = true) {
+    url = API_BASIC_URL + url;
+    let token: string | null = null;
+    if (withToken) {
+      token = await getToken(url, false).then((token) => token);
+    }
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token as string,
+        },
+      })
+        .then(async (response) => {
+          const result = (await response.json()) as any;
+          if (result.code != "0") {
+            toast.error(result?.message);
+            reject(result);
+          } else {
+            resolve(result);
+          }
+        })
+        .catch((error) => {
+          toast.error(error.message);
+          reject(error);
+        });
+    });
+  }
 
-async function get(url) {
-  url = API_BASIC_URL + url;
-  const token = await getToken(url, false).then((token) => token);
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token as string,
-    },
-  }).then((response) => {
-    if (!response.ok) throw new Error("HTTP状态异常: " + response.status); // 检查响应状态‌:ml-citation{ref="5" data="citationList"}
-    return response.json();
-  });
+  static async post(url, body, withToken = true) {
+    url = API_BASIC_URL + url;
+    let token: string | null = null;
+    if (withToken) {
+      token = await getToken("", false).then((token) => token);
+    }
+
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token as string,
+        },
+        body: body,
+      })
+        .then(async (response) => {
+          const result = (await response.json()) as any;
+          if (result.code != "0") {
+            toast.error(result?.message);
+            reject(result);
+          } else {
+            resolve(result);
+          }
+        })
+        .catch((error) => {
+          toast.error(error.message);
+          reject(error);
+        });
+    });
+  }
 }
 
-async function post(url, body) {
-  url = API_BASIC_URL + url;
-  const token = await getToken("", false).then((token) => token);
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token as string,
-    },
-    body: body,
-  }).then((response) => {
-    if (!response.ok) throw new Error("HTTP状态异常: " + response.status); // 检查响应状态‌:ml-citation{ref="5" data="citationList"}
-    return response.json();
-  });
-}
-
-export { get, post };
+export { Fetcher };
