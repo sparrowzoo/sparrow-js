2d4ffe68cc5837119025873d762a0f4221bfcc5e
张立志 <zhanglizhi@tedu.cn>
2025-03-21 23:06:20 +0800

    build

--- react-next-14/src/components/ChatLayout.tsx
+++ react-next-14/src/components/ChatLayout.tsx
@@ -11,7 +11,7 @@ export default function ChatLayout({
     <div className="flex flex-row flex-1 w-full">
       <div className="w-[20rem] flex flex-col  gap-4 p-4 border-r border-indigo-500">
         <div className={"bg-cyan-500 text-white p-2 rounded-xl cursor-pointer"}>
-          <Link href={`${NEXT_ASSET_PREFIX}/chat`}>通讯录管理</Link>
+          <Link href={`${NEXT_ASSET_PREFIX}/chat/friends`}>通讯录管理</Link>
         </div>
         <div className={"bg-cyan-500 text-white p-2 rounded-xl cursor-pointer"}>
           <Link href={`${NEXT_ASSET_PREFIX}/chat/sessions/session`}>
--- react-next-14/src/components/GroupItem.tsx
+++ react-next-14/src/components/GroupItem.tsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
 import * as React from "react";
+import { NEXT_ASSET_PREFIX } from "@/lib/EnvUtils";
 
 interface GroupProps {
   groupId: string;
 }
 
 export default function GroupItem(groupProps: GroupProps) {
-  const headSrc = `/columns/${groupProps.groupId}.jpg`;
-  const groupUrl = `/chat/friends/group?groupId=${groupProps.groupId}`;
-  return <div className="flex items-center text-left">
-    <Link href={groupUrl}>
-      <img className={"w-16 h-16 rounded-full mr-4"} src={headSrc} /></Link>
-    <div>
+  const headSrc = `${NEXT_ASSET_PREFIX}/columns/${groupProps.groupId}.jpg`;
+  const groupUrl = `${NEXT_ASSET_PREFIX}/chat/friends/group?groupId=${groupProps.groupId}`;
+  return (
+    <div className="flex items-center text-left">
       <Link href={groupUrl}>
-        <strong>Andrew Alfred</strong><br/>
-        <span>Technical advisor</span>
+        <img className={"w-16 h-16 rounded-full mr-4"} src={headSrc} />
       </Link>
+      <div>
+        <Link href={groupUrl}>
+          <strong>Andrew Alfred</strong>
+          <br />
+          <span>Technical advisor</span>
+        </Link>
+      </div>
     </div>
-  </div>;
+  );
 }
