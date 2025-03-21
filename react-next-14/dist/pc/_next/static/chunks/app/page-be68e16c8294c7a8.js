919b4640d8036122f2abaf78f656aac870ca9a92
张立志 <zhanglizhi@tedu.cn>
2025-03-21 21:02:22 +0800

    add react demo

---
+++ react-next-14/dist/pc/_next/static/chunks/app/page-be68e16c8294c7a8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1931,5964,8046],{14259:function(n,e,u){Promise.resolve().then(u.t.bind(u,38173,23))}},function(n){n.O(0,[8173,2971,7023,1744],function(){return n(n.s=14259)}),_N_E=n.O()}]);
