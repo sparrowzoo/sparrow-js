c9df61447d1897afa14a8ae13d4057ff21c87b6f
张立志 <zhanglizhi@tedu.cn>
2025-03-23 21:57:22 +0800

    add react demo

--- react-next-14/src/app/chat/friends/layout.tsx
+++ react-next-14/src/app/chat/friends/layout.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Contacts from "@/components/Contacts";
-import Groups from "@/components/Groups";
 
 export default function ChatLayout({
   children,
@@ -11,7 +10,8 @@ export default function ChatLayout({
     <div className="flex flex-row">
       <div className="w-[20rem] flex flex-col p-2 gap-4 border-gray-300">
         <Contacts currentUserId={"1"} />
-        <Groups groupId={"1"} />
+        <Contacts currentUserId={"2"} />
+        {/*<Groups groupId={"1"} />*/}
       </div>
       <div className={"flex-1 border-2 border-gray-300"}>{children}</div>
     </div>
