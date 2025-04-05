c12d898b20e888d45fce60c5617e2355230e5f4d
张立志 <zhanglizhi@tedu.cn>
2025-04-05 10:14:00 +0800

    modify 验证码

---
+++ react-next-14/src/app/chat/login/page.tsx
@@ -0,0 +1,13 @@
+import Login from "@/components/Login";
+
+export default function LoginPage() {
+  return (
+    <div className={"w-full"}>
+      <h1>登录</h1>
+      <h2>内部测试使用</h2>
+      <p>
+        <Login />
+      </p>
+    </div>
+  );
+}
