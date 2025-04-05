5f293494d592cb5442e86a180fb93133474baf89
张立志 <zhanglizhi@tedu.cn>
2025-04-05 10:17:52 +0800

    modify 验证码

---
+++ react-next-14/src/app/chat/page.tsx
@@ -0,0 +1,14 @@
+// const nextConfig = {
+//   async redirects() {
+//     return [
+//       // Basic redirect
+//       {
+//         source: '/chat',
+//         destination: '/chat/friends',
+//         permanent: true,
+//       }
+//     ]
+//   }
+export default function ChatPage() {
+  return <div>Chat Page</div>
+}
