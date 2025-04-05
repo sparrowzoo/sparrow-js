035f7cddf9a474c6a930ddd47dc7ce985e9e2c21
张立志 <zhanglizhi@tedu.cn>
2025-04-05 09:38:12 +0800

    merge

--- react-next-14/src/common/components/LoadingSpinner.tsx
+++ react-next-14/src/common/components/LoadingSpinner.tsx
@@ -1,8 +1,13 @@
-'use client'
+"use client";
 import React from "react";
-import UseAnimations from "react-useanimations";
 import loading from "react-useanimations/lib/loading";
+import dynamic from "next/dynamic";
+
+const DynamicAnimation = dynamic(
+  () => import("react-useanimations"),
+  { ssr: false } // 禁用服务端渲染
+);
 
 export default function LoadingSpinner() {
-  return <UseAnimations animation={loading} size={56} />;
+  return <DynamicAnimation animation={loading} size={56} />;
 }
