0fc7e5e97d6992b4f467b3f532c98c122f31e8c2
张立志 <zhanglizhi@tedu.cn>
2025-04-09 19:41:27 +0800

    modify 验证码

--- react-next-shadcn/postcss.config.mjs
+++ react-next-shadcn/postcss.config.mjs
@@ -1,13 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
-    plugins: {
-        'postcss-import': {},
-        'tailwindcss/nesting': {},//默认是postcss-nested 更好
-        tailwindcss: {},
-        autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production' ? {cssnano: {}} : {})
-        //https://tailwindcss.com/docs/optimizing-for-production
-    },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
---
+++ react-next-shadcn/src/app/sidebar/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function Home() {
+  return <div className="flex"></div>;
+}
