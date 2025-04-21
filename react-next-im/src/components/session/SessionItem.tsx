153a5e62aca31345d0c456278406c5023e254e1c
张立志 <zhanglizhi@tedu.cn>
2025-04-21 14:05:46 +0800

    add contact server

--- react-next-im/src/components/session/SessionItem.tsx
+++ react-next-im/src/components/session/SessionItem.tsx
@@ -1,9 +1,7 @@
 "use client";
 import * as React from "react";
-import { SidebarMenuBadge, SidebarMenuItem } from "@/components/ui/sidebar";
+import { SidebarMenuItem } from "@/components/ui/sidebar";
 import SessionItemTrigger from "@/components/session/SessionItemTrigger";
-import { Position } from "@/lib/protocol/ItemProps";
-import { Badge } from "@/components/ui/badge";
 import SessionItemProps from "@/components/session/item/SessionItemProps";
 
 export default function SessionItem(props: SessionItemProps) {
@@ -16,20 +14,6 @@ export default function SessionItem(props: SessionItemProps) {
         triggerType={props.triggerType}
         chatSession={chatSession}
       />
-      <>
-        {chatSession.unreadCount} {chatSession.unreadPosition}
-      </>
-      {chatSession.unreadCount > 0 && props.unreadPosition == Position.TAIL && (
-        <SidebarMenuBadge>
-          <Badge
-            className={
-              "absolute bg-red-500 p-1  text-white z-50 font-bold rounded-full"
-            }
-          >
-            {chatSession.unreadCount > 99 ? "99+" : chatSession.unreadCount}
-          </Badge>
-        </SidebarMenuBadge>
-      )}
     </SidebarMenuItem>
   );
 }
--- react-next-im/src/components/session/item/BaseTrigger.tsx
+++ react-next-im/src/components/session/item/BaseTrigger.tsx
@@ -2,28 +2,47 @@ import SessionItemProps from "@/components/session/item/SessionItemProps";
 import MyAvatar from "@/components/MyAvatar";
 import { Position } from "@/lib/protocol/ItemProps";
 import * as React from "react";
+import { Badge } from "@/components/ui/badge";
 
 export default function BaseTrigger(sessionItemProps: SessionItemProps) {
   const chatSession = sessionItemProps.chatSession;
   return (
-    <div className="flex flex-row h-10">
+    <div className="flex flex-row h-10 w-full">
       <MyAvatar
         unread={chatSession.unreadCount}
         showUnread={chatSession.unreadPosition == Position.HEADER}
         fallback={chatSession.name as string}
         src={chatSession.avatarUrl}
       />
-      <div className={"flex flex-1 flex-col justify-start items-center ml-2"}>
-        <div className={"flex flex-row w-full text-xs justify-between"}>
-          <span className={"text-xs"}>{chatSession.name}</span>
+      <div
+        className={
+          "flex flex-1 flex-col justify-start items-center ml-2 text-xs"
+        }
+      >
+        <div className={"flex flex-row justify-between items-between w-full"}>
+          <span>{chatSession.name}</span>
           <span>{chatSession.lastMessage?.sendTime}</span>
         </div>
-        <span
-          title={chatSession.lastMessage?.content}
-          className={"text-gray-400 text-left text-xs truncate  w-[10rem]"}
-        >
-          {chatSession.lastMessage?.content}
-        </span>
+        <div className={"flex flex-row justify-between items-between w-full"}>
+          <span
+            title={chatSession.lastMessage?.content}
+            className={
+              "text-gray-400 flex-1 text-left text-xs truncate  w-[10rem]"
+            }
+          >
+            {chatSession.lastMessage?.content}
+          </span>
+          {chatSession.unreadCount > 0 &&
+            sessionItemProps.unreadPosition == Position.TAIL && (
+              <Badge
+                className={
+                  "bg-red-500 p-1 w-5 h-5 text-xs text-center  text-white z-50 font-bold rounded-full"
+                }
+              >
+                {chatSession.unreadCount > 99 ? "99+" : chatSession.unreadCount}
+              </Badge>
+            )}
+        </div>
       </div>
     </div>
   );
