f22c713686a4641335fba43ea87dc2ac8976166d
张立志 <zhanglizhi@tedu.cn>
2025-04-22 09:59:33 +0800

    add contact server

--- react-next-im/src/app/(im)/12talk/Demo.tsx
+++
@@ -1,8 +0,0 @@
-export default function Demo() {
-  return (
-    <div>
-      <h1>Welcome to 12talk</h1>
-      <p>This is a demo page for 12talk.</p>
-    </div>
-  );
-}
--- react-next-im/src/app/(im)/12talk/page.tsx
+++ react-next-im/src/app/(im)/12talk/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import dynamic from "next/dynamic";
 import { createRoot } from "react-dom/client";
 
-const JQueryComponent = dynamic(() => import("./back"), {
+const JQuery12Talk = dynamic(() => import("./jQuery12Talk"), {
   ssr: false, // 仅客户端渲染
 });
 export default function Page() {
@@ -12,7 +12,7 @@ export default function Page() {
     const container = document.getElementById("content-container");
     if (container) {
       const root = createRoot(container);
-      root.render(<JQueryComponent />);
+      root.render(<JQuery12Talk />);
     }
   }, []);
   return null; // 返回空值，避免自动生成默认 div
