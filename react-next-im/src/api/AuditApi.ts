91f3d4109a2b9e12aa240d75fc783e1ba3fcd031
张立志 <zhanglizhi@tedu.cn>
2025-04-29 01:17:24 +0800

    20250428上线

--- react-next-im/src/api/AuditApi.ts
+++ react-next-im/src/api/AuditApi.ts
@@ -5,42 +5,50 @@ import AuditWrap from "@/lib/protocol/audit/AuditWrap";
 import toast from "react-hot-toast";
 
 export default class AuditApi {
-    public static async getFriendAuditList(): Promise<any> {
-        let auditWrap: AuditWrap|null = null;
-        await Fetcher.get("/audit/friend-apply-list.json", CrosStorage.getCrosStorage()).then(
-            (response: Result) => {
-                const remoteAuditList: AuditWrap = response.data;
-                auditWrap = remoteAuditList;
-            }
-        );
-        return auditWrap;
-    }
+  public static async getFriendAuditList(): Promise<any> {
+    let auditWrap: AuditWrap | null = null;
+    await Fetcher.get(
+      "/audit/friend-apply-list.json",
+      CrosStorage.getCrosStorage()
+    ).then((response: Result) => {
+      const remoteAuditList: AuditWrap = response.data;
+      auditWrap = remoteAuditList;
+    });
+    return auditWrap;
+  }
 
-    public static async getGroupAuditList(): Promise<any> {
-        let auditWrap: AuditWrap|null = null;
-        await Fetcher.get("/audit/qun-member-apply-list.json", CrosStorage.getCrosStorage()).then(
-            (response: Result) => {
-                const remoteAuditList: AuditWrap = response.data;
-                auditWrap = remoteAuditList;
-            }
-        );
-        return auditWrap;
-    }
+  public static async getGroupAuditList(): Promise<any> {
+    let auditWrap: AuditWrap | null = null;
+    await Fetcher.get(
+      "/audit/qun-member-apply-list.json",
+      CrosStorage.getCrosStorage()
+    ).then((response: Result) => {
+      const remoteAuditList: AuditWrap = response.data;
+      auditWrap = remoteAuditList;
+    });
+    return auditWrap;
+  }
 
-
-    public static async auditQunMember(auditId:number,agree:boolean): Promise<any> {
-        let audit={
-            agree: agree,
-            auditId: auditId,
-            reason: ""
-        };
-        const body=JSON.stringify(audit);
-        await Fetcher.post("/audit/audit-qun-apply.json",body, CrosStorage.getCrosStorage()).then(
-            (response: Result) => {
-                toast.success("操作成功");
-            }
-        ).catch((error: any) => {
-            console.log(error);
-        });
-    }
+  public static async auditQunMember(
+    auditId: number,
+    agree: boolean
+  ): Promise<any> {
+    let audit = {
+      isAgree: agree,
+      auditId: auditId,
+      reason: "",
+    };
+    const body = JSON.stringify(audit);
+    await Fetcher.post(
+      "/audit/audit-qun-apply.json",
+      body,
+      CrosStorage.getCrosStorage()
+    )
+      .then((response: Result) => {
+        toast.success("操作成功");
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }
 }
