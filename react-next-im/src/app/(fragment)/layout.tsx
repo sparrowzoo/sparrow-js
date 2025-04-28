4e1d9b57d8d2b5f4b8c02588ed14997e853b5a3c
张立志 <zhanglizhi@tedu.cn>
2025-04-28 16:37:38 +0800

    merge

--- react-next-im/src/app/(fragment)/layout.tsx
+++ react-next-im/src/app/(fragment)/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../globals.css";
 import * as React from "react";
 
 /**
--- react-next-im/src/app/(index)/layout.tsx
+++ react-next-im/src/app/(index)/layout.tsx
@@ -1,4 +1,4 @@
-import "../globals.css";
+import "@/app/globals.css";
 import { Metadata, Viewport } from "next";
 import * as React from "react";
 import { Toaster } from "react-hot-toast";
