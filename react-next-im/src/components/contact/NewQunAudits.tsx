cafc4242889c9cc8de8c927838e39e1eed0d9caa
张立志 <zhanglizhi@tedu.cn>
2025-04-29 11:31:55 +0800

    20250429上线 生产环境token 写入local storage

--- react-next-im/src/components/contact/NewQunAudits.tsx
+++ react-next-im/src/components/contact/NewQunAudits.tsx
@@ -1,29 +1,43 @@
-import NewFriendItem from "@/components/contact/NewFriendItem";
-import {SidebarMenu} from "@/components/ui/sidebar";
+import { SidebarMenu } from "@/components/ui/sidebar";
 import NewQunAuditItem from "@/components/contact/NewQunAuditItem";
-import {useEffect, useState} from "react";
+import * as React from "react";
+import { useContext, useEffect, useState } from "react";
 import AuditWrap from "@/lib/protocol/audit/AuditWrap";
 import AuditApi from "@/api/AuditApi";
 import ThreeDotLoading from "@/common/components/ThreeDotLoading";
-import * as React from "react";
+import { WebSocketContext } from "@/lib/im/WebSocketProvider";
 
 export default function NewQunAudits() {
-    const [auditWrap, setAuditWrap] = useState<AuditWrap>();
-    useEffect(() => {
-        AuditApi.getGroupAuditList().then(setAuditWrap);
-    }, []);
-    if (!auditWrap) {
-        return <ThreeDotLoading />;
-    }
-    return (
-        <SidebarMenu className={"gap-2 w-full"}>
-            {auditWrap.auditingList.map((audit) => (
-                <NewQunAuditItem key={audit.auditId} audit={audit} auditWrap={auditWrap} />))
-            }
+  const webSocketContextValue = useContext(WebSocketContext);
+  const messageBroker = webSocketContextValue.messageBroker;
 
-            {auditWrap.myApplyingList.map((audit) => (
-                <NewQunAuditItem key={audit.auditId} audit={audit} auditWrap={auditWrap} />))
-            }
-        </SidebarMenu>
-    )
+  const [auditWrap, setAuditWrap] = useState<AuditWrap>();
+  useEffect(() => {
+    AuditApi.getGroupAuditList().then((auditWrap) => {
+      setAuditWrap(auditWrap);
+      messageBroker.contactContainer.putJsonContact(auditWrap.contactMap);
+    });
+  }, []);
+  if (!auditWrap) {
+    return <ThreeDotLoading />;
+  }
+  return (
+    <SidebarMenu className={"gap-2 w-full"}>
+      {auditWrap.auditingList.map((audit) => (
+        <NewQunAuditItem
+          key={audit.auditId}
+          audit={audit}
+          auditWrap={auditWrap}
+        />
+      ))}
+
+      {auditWrap.myApplyingList.map((audit) => (
+        <NewQunAuditItem
+          key={audit.auditId}
+          audit={audit}
+          auditWrap={auditWrap}
+        />
+      ))}
+    </SidebarMenu>
+  );
 }
