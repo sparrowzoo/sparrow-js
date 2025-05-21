6c86ade90b8d324e0b3ac83bdbb9c9a8bf531336
张立志 <zhanglizhi@tedu.cn>
2025-05-21 22:31:46 +0800

    add shallow =true

--- common/src/common/components/Draggable.tsx
+++ common/src/common/components/Draggable.tsx
@@ -1,46 +1,69 @@
 "use client";
-import { DndContext, useDraggable } from "@dnd-kit/core";
+import { DndContext, DragStartEvent, useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Move } from "lucide-react";
 
 interface DraggableContainerProps {
-  x: number;
-  y: number;
-  dragging: boolean;
+  position: Position;
+  dragged: boolean;
   children: React.ReactNode;
+  setInitPosition: any;
 }
-
-let initPosition = {};
 
 function DraggableContainer(draggableProps: DraggableContainerProps) {
   const draggingRef = React.useRef<HTMLDivElement>(null);
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: "box",
-  });
-  useEffect(() => {}, []);
-  const { children, dragging, x, y } = draggableProps;
-  let mergedStyles = {};
-  if (transform) {
-    transform.x += x;
-    transform.y += y;
-    mergedStyles = {
-      transform: CSS.Transform.toString(transform),
-    };
-  } else {
-    mergedStyles = {
-      position: "fixed",
-      left: `${x}px`,
-      top: `${y}px`,
-    };
+  const [init, setInit] = useState(false);
+  const { attributes, listeners, setNodeRef, transform, isDragging } =
+    useDraggable({
+      id: "box",
+    });
+
+  function initPosition() {
+    if (!init) {
+      const container = document.getElementById("box");
+      const rect = container.getBoundingClientRect();
+      const position = {
+        left: rect.left,
+        top: rect.top,
+      };
+      debugger;
+      console.log("dragging init " + JSON.stringify(position));
+      setInitPosition(position);
+      container.style.position = "absolute";
+      container.style.left = `${position.left}px`;
+      container.style.top = `${position.top}px`;
+      //setInit(true);
+    }
   }
 
+  const { children, dragged, position, setInitPosition } = draggableProps;
+  let mergedStyles = {};
+  if (transform) {
+    transform.x += position?.left;
+    transform.y += position?.top;
+    mergedStyles = {
+      transform: CSS.Transform.toString(transform),
+      position: "transform",
+    };
+  } else {
+    if (position && init) {
+      mergedStyles = {
+        position: "fixed",
+        left: `${position.left}px`,
+        top: `${position.top}px`,
+      };
+    }
+  }
+
+  console.log("dragging init function ....", JSON.stringify(mergedStyles));
   return (
     <div
       onMouseOver={() => {
         if (draggingRef.current) {
           draggingRef.current.style.visibility = "visible";
         }
+        initPosition();
       }}
       onMouseOut={() => {
         if (draggingRef.current) {
@@ -56,7 +79,11 @@ function DraggableContainer(draggableProps: DraggableContainerProps) {
       }}
       {...attributes}
     >
-      <div ref={draggingRef} className={"invisible"} {...listeners}>
+      <div
+        ref={draggingRef}
+        className={"invisible flex flex-row"}
+        {...listeners}
+      >
         <Move />
         drag me
       </div>
@@ -69,24 +96,36 @@ interface DraggableProps {
   children: React.ReactNode;
 }
 
+type Position = { left: number; top: number } | null;
+
 export default function Draggable(props: DraggableProps) {
-  const [fixedPosition, setFixedPosition] = useState({ left: 0, top: 0 });
-  const [dragging, setDragging] = useState(false);
+  const [fixedPosition, setFixedPosition] = useState<Position>({
+    left: 0,
+    top: 0,
+  });
+  const [dragged, setDragged] = useState(false);
+
   return (
     <DndContext
-      onDragStart={() => {
-        console.log("drag start");
+      onDragStart={(e: DragStartEvent) => {
+        console.log("dragging start " + JSON.stringify(fixedPosition));
       }}
       onDragEnd={(e) => {
         setFixedPosition((prev) => {
-          return { left: prev.left + e.delta.x, top: prev.top + e.delta.y };
+          const left = prev?.left;
+          const top = prev?.top;
+          if (left && top) {
+            return { left: left + e.delta.x, top: top + e.delta.y };
+          }
+          return { left: e.delta.x, top: e.delta.y };
         });
+        setDragged(true);
       }}
     >
       <DraggableContainer
-        x={fixedPosition.left}
-        y={fixedPosition.top}
-        dragging={dragging}
+        position={fixedPosition}
+        setInitPosition={setFixedPosition}
+        dragged={dragged}
       >
         {props.children}
       </DraggableContainer>
