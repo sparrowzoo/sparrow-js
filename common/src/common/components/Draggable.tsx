1681dd109233f1c1dcbd9452b5a5ef3ef5fa9395
zh_harry <zh_harry@163.com>
2025-05-21 23:50:51 +0800

    move chat

--- common/src/common/components/Draggable.tsx
+++ common/src/common/components/Draggable.tsx
@@ -13,41 +13,25 @@ interface DraggableContainerProps {
 
 function DraggableContainer(draggableProps: DraggableContainerProps) {
   const draggingRef = React.useRef<HTMLDivElement>(null);
-  const [init, setInit] = useState(false);
   const { attributes, listeners, setNodeRef, transform, isDragging } =
-    useDraggable({
-      id: "box",
-    });
+      useDraggable({
+        id: "box",
+      });
 
-  function initPosition() {
-    if (!init) {
-      const container = document.getElementById("box");
-      const rect = container.getBoundingClientRect();
-      const position = {
-        left: rect.left,
-        top: rect.top,
-      };
-      debugger;
-      console.log("dragging init " + JSON.stringify(position));
-      setInitPosition(position);
-      container.style.position = "absolute";
-      container.style.left = `${position.left}px`;
-      container.style.top = `${position.top}px`;
-      //setInit(true);
-    }
-  }
 
   const { children, dragged, position, setInitPosition } = draggableProps;
   let mergedStyles = {};
   if (transform) {
-    transform.x += position?.left;
-    transform.y += position?.top;
+    if(position) {
+      transform.x += position?.left;
+      transform.y += position?.top;
+    }
     mergedStyles = {
       transform: CSS.Transform.toString(transform),
       position: "transform",
     };
   } else {
-    if (position && init) {
+    if (position) {
       mergedStyles = {
         position: "fixed",
         left: `${position.left}px`,
@@ -58,37 +42,37 @@ function DraggableContainer(draggableProps: DraggableContainerProps) {
 
   console.log("dragging init function ....", JSON.stringify(mergedStyles));
   return (
-    <div
-      onMouseOver={() => {
-        if (draggingRef.current) {
-          draggingRef.current.style.visibility = "visible";
-        }
-        initPosition();
-      }}
-      onMouseOut={() => {
-        if (draggingRef.current) {
-          draggingRef.current.style.visibility = "hidden";
-        }
-      }}
-      className={"w-fit h-fit"}
-      id={"box"}
-      ref={setNodeRef}
-      style={{
-        cursor: "grab",
-        ...mergedStyles,
-      }}
-      {...attributes}
-    >
-      <div
-        ref={draggingRef}
-        className={"invisible flex flex-row"}
-        {...listeners}
+      <div id={"draggable-container"}
+           onMouseOver={() => {
+             if (draggingRef.current) {
+               draggingRef.current.style.visibility = "visible";
+             }
+           }}
+           onMouseOut={() => {
+             if (draggingRef.current) {
+               draggingRef.current.style.visibility = "hidden";
+             }
+           }}
+           className={"w-fit h-fit"}
+           ref={setNodeRef}
+           style={{
+             cursor: "grab",
+             ...mergedStyles,
+           }}
+           {...attributes}
       >
-        <Move />
-        drag me
+        <div
+            ref={draggingRef}
+            className={"invisible flex flex-row"}
+            {...listeners}
+        >
+
+
+          <Move />
+          drag me
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
   );
 }
 
@@ -106,29 +90,29 @@ export default function Draggable(props: DraggableProps) {
   const [dragged, setDragged] = useState(false);
 
   return (
-    <DndContext
-      onDragStart={(e: DragStartEvent) => {
-        console.log("dragging start " + JSON.stringify(fixedPosition));
-      }}
-      onDragEnd={(e) => {
-        setFixedPosition((prev) => {
-          const left = prev?.left;
-          const top = prev?.top;
-          if (left && top) {
-            return { left: left + e.delta.x, top: top + e.delta.y };
-          }
-          return { left: e.delta.x, top: e.delta.y };
-        });
-        setDragged(true);
-      }}
-    >
-      <DraggableContainer
-        position={fixedPosition}
-        setInitPosition={setFixedPosition}
-        dragged={dragged}
+      <DndContext
+          onDragStart={(e: DragStartEvent) => {
+            console.log("dragging start " + JSON.stringify(fixedPosition));
+          }}
+          onDragEnd={(e) => {
+            setFixedPosition((prev) => {
+              const left = prev?.left;
+              const top = prev?.top;
+              if (left && top) {
+                return { left: left + e.delta.x, top: top + e.delta.y };
+              }
+              return { left: e.delta.x, top: e.delta.y };
+            });
+            setDragged(true);
+          }}
       >
-        {props.children}
-      </DraggableContainer>
-    </DndContext>
+        <DraggableContainer
+            position={fixedPosition}
+            setInitPosition={setFixedPosition}
+            dragged={dragged}
+        >
+          {props.children}
+        </DraggableContainer>
+      </DndContext>
   );
 }
--- react-next-im/src/app/(example)/draggable/more/page.tsx
+++ react-next-im/src/app/(example)/draggable/more/page.tsx
@@ -1,57 +1,52 @@
 "use client";
 import React from "react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
-import Draggable from "@/common/components/Draggable";
-import { Sidebar, SidebarMenu, SidebarProvider } from "@/components/ui/sidebar";
+import {Popover, PopoverContent, PopoverTrigger,} from "@/components/ui/popover";
+import {Button} from "@/components/ui/button";
+import {Sidebar, SidebarMenu, SidebarProvider} from "@/components/ui/sidebar";
 
 function DraggableItem() {
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="outline">Open popover</Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        <Draggable>
-          <div className="bg-gray-200 p-4 w-[200px] h-[200px]">hello</div>
-        </Draggable>
-      </PopoverContent>
-    </Popover>
-  );
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant="outline">Open popover</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className="bg-gray-200 p-4 w-[200px] h-[200px]">hello</div>
+            </PopoverContent>
+        </Popover>
+    );
 }
 
 function Talk() {
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="outline">Open popover</Button>
-      </PopoverTrigger>
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant="outline">Open popover</Button>
+            </PopoverTrigger>
 
-      <PopoverContent>
-        <SidebarProvider className={"min-h-full h-full w-auto"}>
-          <Sidebar className={"relative min-h-full h-full"}>
-            <SidebarMenu>
-              <DraggableItem />
-              <DraggableItem />
-              <DraggableItem />
-            </SidebarMenu>
-          </Sidebar>
-        </SidebarProvider>
-      </PopoverContent>
-    </Popover>
-  );
+            <PopoverContent>
+                <SidebarProvider className={"min-h-full h-full w-auto"}>
+                    <Sidebar className={"relative min-h-full h-full"}>
+                        <SidebarMenu>
+                            <DraggableItem/>
+                            <DraggableItem/>
+                            <DraggableItem/>
+                        </SidebarMenu>
+                    </Sidebar>
+                </SidebarProvider>
+            </PopoverContent>
+        </Popover>
+    );
 }
 
 export default function App() {
-  return (
-    <>
-      <div className="fixed  right-100 bottom-0">
-        <Talk />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1>这里和shandcn 组件有冲突 父组件fixed transform 会影响子组件fixed 定位</h1>
+            <div style={{height: "100vh", width: "100vw", position: "fixed", top: 636, left: 516, zIndex: -1}}>222</div>
+            <div className="fixed  right-100 bottom-0">
+                <Talk/>
+            </div>
+        </>
+    );
 }
