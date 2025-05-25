4640b9873485b82846c5018a94a0c0f9e394bb13
张立志 <zhanglizhi@tedu.cn>
2025-05-25 19:28:32 +0800

    add shallow =true

--- react-next-im/public/imInstaller.js
+++ react-next-im/public/imInstaller.js
@@ -57,23 +57,23 @@ loadjQuery(function () {
       $(this).clone().appendTo("head");
     });
     // 处理脚本（保持顺序执行）
-    // const scripts = container.find("script").get();
-    // (function loadScript(index) {
-    //   if (index >= scripts.length) return;
-    //
-    //   const script = scripts[index];
-    //   const newScript = $("<script>");
-    //
-    //   if (script.src) {
-    //     newScript
-    //       .attr("src", script.src + "?t=" + Date.now())
-    //       .on("load", () => loadScript(index + 1));
-    //   } else {
-    //     newScript.text(script.innerHTML);
-    //     setTimeout(() => loadScript(index + 1), 0);
-    //   }
-    //   $("body").append(newScript);
-    // })(0);
+    const scripts = container.find("script").get();
+    (function loadScript(index) {
+      if (index >= scripts.length) return;
+
+      const script = scripts[index];
+      const newScript = $("<script>");
+
+      if (script.src) {
+        newScript
+          .attr("src", script.src + "?t=" + Date.now())
+          .on("load", () => loadScript(index + 1));
+      } else {
+        newScript.text(script.innerHTML);
+        setTimeout(() => loadScript(index + 1), 0);
+      }
+      $("body").append(newScript);
+    })(0);
   }
 
   $(document).ready(() => {
