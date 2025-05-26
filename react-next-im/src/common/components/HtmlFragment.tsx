102ce2ea734d4a68b94958702a906d460adc9539
张立志 <zhanglizhi@tedu.cn>
2025-05-26 09:21:39 +0800

    add shallow =true

--- react-next-im/src/common/components/HtmlFragment.tsx
+++ react-next-im/src/common/components/HtmlFragment.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import Hello from "@/components/Hello";
 
 interface HtmlFragmentProps {
   DynamicComponent: React.ComponentType<any>;
@@ -10,13 +9,12 @@ interface HtmlFragmentProps {
 export default function HtmlFragment(htmlFragmentProps: HtmlFragmentProps) {
   const { DynamicComponent } = htmlFragmentProps;
   useEffect(() => {
-    debugger;
     // 手动挂载到指定容器
     const container = document.getElementById(htmlFragmentProps.containerId);
     if (container) {
       const root = createRoot(container);
-      root.render(<Hello />);
+      root.render(<DynamicComponent />);
     }
   }, []);
-  return <></>; // 返回空值，避免自动生成默认 div
+  return null; // 返回空值，避免自动生成默认 div
 }
--- react-next-im/src/common/i18n/request.ts
+++ react-next-im/src/common/i18n/request.ts
@@ -5,7 +5,6 @@ import { routing } from "@/i18n/routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // Typically corresponds to the `[locale]` segment
-  console.log("requestLocale", requestLocale);
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
