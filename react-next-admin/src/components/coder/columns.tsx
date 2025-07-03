937f54bc06fedeb45729020c329e5750946ab457
张立志 <zhanglizhi@tedu.cn>
2025-07-03 21:27:01 +0800

    add shallow =true

--- react-next-admin/src/components/coder/columns.tsx
+++ react-next-admin/src/components/coder/columns.tsx
@@ -5,88 +5,153 @@ import NormalCell from "@/common/components/table/cell/normal";
 import PlainTextHeader from "@/common/components/table/header/plain-text";
 
 export interface ColumnConfig extends BasicData<ColumnConfig> {
- tableClassName?: string;
- propertyName?: string;
- chineseName?: string;
- subsidiaryColumns?: string;
- javaType?: string;
- enableHidden?: boolean;
- defaultHidden?: boolean;
- showInEdit?: boolean;
- showInList?: boolean;
- showInSearch?: boolean;
- allowNull?: boolean;
- placeholder?: string;
- defaultValue?: string;
- /** 查询方式 */
- searchType?: number;
- validateType?: string;
- validator?: string; // 需替换为具体的Validator类型
- dataSourceType?: number;
- dataSourceParams?: string;
- columnType?: number;
- headerType?: number;
- cellType?: number;
- controlType?: number;
- sort?: number;
- readOnly?: boolean;
+    tableClassName: string;
+    propertyName: string;
+    chineseName: string;
+    subsidiaryColumns: string;
+    javaType: string;
+    enableHidden: boolean;
+    defaultHidden: boolean;
+    showInEdit: boolean;
+    showInList: boolean;
+    showInSearch: boolean;
+    allowNull: boolean;
+    placeholder: string;
+    defaultValue: string;
+    searchType: number;
+    validateType: string;
+    validator: any;
+    dataSourceType: number;
+    dataSourceParams: string;
+    columnType: number;
+    headerType: number;
+    cellType: number;
+    controlType: number;
+    sort: number;
+    readOnly: boolean;
 }
 
 
 export const columns: ColumnDef<ColumnConfig>[] = [
-{
-  accessorKey: "id",
-  header: PlainTextHeader({ columnTitle: "ID" } as ColumnOperationProps),
-  cell: NormalCell("id"),
-  enableHiding: true
- },
- {
-  accessorKey: "propertyName",
-  header: PlainTextHeader({ columnTitle: "属性名" } as ColumnOperationProps),
-  cell: NormalCell("propertyName"),
- },
- {
-  accessorKey: "chineseName",
-  header: PlainTextHeader({ columnTitle: "中文名" } as ColumnOperationProps),
-  cell: NormalCell("chineseName"),
- },
- {
-  accessorKey: "javaType",
-  header: SelectHeader({
-   columnTitle: "字段类型",
-   options: ["String", "Integer", "Boolean", "Date"]
-  } as ColumnOperationProps),
-  cell: TagCell("javaType"),
-  columnType: 2,
-  sort: 3
- },
- {
-  accessorKey: "defaultValue",
-  header: PlainTextHeader({ columnTitle: "默认值" } as ColumnOperationProps),
-  cell: EditableCell("defaultValue"),
-  showInEdit: true,
-  sort: 4
- },
- {
-  accessorKey: "readOnly",
-  header: SwitchHeader({ columnTitle: "只读" } as ColumnOperationProps),
-  cell: BooleanCell("readOnly"),
-  controlType: 3,
-  sort: 5
- },
- {
-  accessorKey: "actions",
-  header: "",
-  cell: ActionCell([
-   {
-    icon: "edit",
-    handler: (row) => handleEdit(row.original)
-   },
-   {
-    icon: "delete",
-    handler: (row) => handleDelete(row.original.id)
-   }
-  ]),
-  enableSorting: false
- }
+    {
+        accessorKey: "tableClassName",
+        header: PlainTextHeader({columnTitle: "类名"} as ColumnOperationProps),
+        cell: NormalCell("tableClassName"),
+        enableHiding: true
+    }, {
+        accessorKey: "propertyName",
+        header: PlainTextHeader({columnTitle: "属性名"} as ColumnOperationProps),
+        cell: NormalCell("propertyName"),
+        enableHiding: true
+    }, {
+        accessorKey: "chineseName",
+        header: PlainTextHeader({columnTitle: "中文名"} as ColumnOperationProps),
+        cell: NormalCell("chineseName"),
+        enableHiding: true
+    }, {
+        accessorKey: "subsidiaryColumns",
+        header: PlainTextHeader({columnTitle: "附加列"} as ColumnOperationProps),
+        cell: NormalCell("subsidiaryColumns"),
+        enableHiding: true
+    }, {
+        accessorKey: "javaType",
+        header: PlainTextHeader({columnTitle: "java类型"} as ColumnOperationProps),
+        cell: NormalCell("javaType"),
+        enableHiding: true
+    }, {
+        accessorKey: "enableHidden",
+        header: PlainTextHeader({columnTitle: "是否隐藏"} as ColumnOperationProps),
+        cell: NormalCell("enableHidden"),
+        enableHiding: true
+    }, {
+        accessorKey: "defaultHidden",
+        header: PlainTextHeader({columnTitle: "默认隐藏"} as ColumnOperationProps),
+        cell: NormalCell("defaultHidden"),
+        enableHiding: true
+    }, {
+        accessorKey: "showInEdit",
+        header: PlainTextHeader({columnTitle: "是否显示在编辑页面"} as ColumnOperationProps),
+        cell: NormalCell("showInEdit"),
+        enableHiding: true
+    }, {
+        accessorKey: "showInList",
+        header: PlainTextHeader({columnTitle: "是否显示在列表页面"} as ColumnOperationProps),
+        cell: NormalCell("showInList"),
+        enableHiding: true
+    }, {
+        accessorKey: "showInSearch",
+        header: PlainTextHeader({columnTitle: "是否显示在搜索页面"} as ColumnOperationProps),
+        cell: NormalCell("showInSearch"),
+        enableHiding: true
+    }, {
+        accessorKey: "allowNull",
+        header: PlainTextHeader({columnTitle: "是否允许为空"} as ColumnOperationProps),
+        cell: NormalCell("allowNull"),
+        enableHiding: true
+    }, {
+        accessorKey: "placeholder",
+        header: PlainTextHeader({columnTitle: "提示信息"} as ColumnOperationProps),
+        cell: NormalCell("placeholder"),
+        enableHiding: true
+    }, {
+        accessorKey: "defaultValue",
+        header: PlainTextHeader({columnTitle: "默认值"} as ColumnOperationProps),
+        cell: NormalCell("defaultValue"),
+        enableHiding: true
+    }, {
+        accessorKey: "searchType",
+        header: PlainTextHeader({columnTitle: "查询方式"} as ColumnOperationProps),
+        cell: NormalCell("searchType"),
+        enableHiding: true
+    }, {
+        accessorKey: "validateType",
+        header: PlainTextHeader({columnTitle: "验证类型"} as ColumnOperationProps),
+        cell: NormalCell("validateType"),
+        enableHiding: true
+    }, {
+        accessorKey: "validator",
+        header: PlainTextHeader({columnTitle: "验证器"} as ColumnOperationProps),
+        cell: NormalCell("validator"),
+        enableHiding: true
+    }, {
+        accessorKey: "dataSourceType",
+        header: PlainTextHeader({columnTitle: "数据源类型"} as ColumnOperationProps),
+        cell: NormalCell("dataSourceType"),
+        enableHiding: true
+    }, {
+        accessorKey: "dataSourceParams",
+        header: PlainTextHeader({columnTitle: "数据源参数"} as ColumnOperationProps),
+        cell: NormalCell("dataSourceParams"),
+        enableHiding: true
+    }, {
+        accessorKey: "columnType",
+        header: PlainTextHeader({columnTitle: "列类型"} as ColumnOperationProps),
+        cell: NormalCell("columnType"),
+        enableHiding: true
+    }, {
+        accessorKey: "headerType",
+        header: PlainTextHeader({columnTitle: "表头类型"} as ColumnOperationProps),
+        cell: NormalCell("headerType"),
+        enableHiding: true
+    }, {
+        accessorKey: "cellType",
+        header: PlainTextHeader({columnTitle: "单元格类型"} as ColumnOperationProps),
+        cell: NormalCell("cellType"),
+        enableHiding: true
+    }, {
+        accessorKey: "controlType",
+        header: PlainTextHeader({columnTitle: "控件类型"} as ColumnOperationProps),
+        cell: NormalCell("controlType"),
+        enableHiding: true
+    }, {
+        accessorKey: "sort",
+        header: PlainTextHeader({columnTitle: "排序"} as ColumnOperationProps),
+        cell: NormalCell("sort"),
+        enableHiding: true
+    }, {
+        accessorKey: "readOnly",
+        header: PlainTextHeader({columnTitle: "是否只读"} as ColumnOperationProps),
+        cell: NormalCell("readOnly"),
+        enableHiding: true
+    }
 ];
