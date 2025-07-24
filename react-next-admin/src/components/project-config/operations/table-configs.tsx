336af787428430009b87e5e58dde4a424d4482af
张立志 <zhanglizhi@tedu.cn>
2025-07-24 21:46:32 +0800

    add shallow =true

---
+++ react-next-admin/src/components/project-config/operations/table-configs.tsx
@@ -0,0 +1,18 @@
+'use client'
+import {CellContextProps, MyTableMeta} from "@/common/lib/table/DataTableProperty";
+import {ProjectConfig} from "@/components/project-config/columns";
+import {useRouter} from "@/common/i18n/navigation";
+import {Button} from "@/components/ui/button";
+import {useTranslations} from "next-intl";
+
+export default function TableConfigs({cellContext}: CellContextProps<ProjectConfig>) {
+    const globalTranslate = useTranslations("GlobalForm");
+    const meta = cellContext.table.options.meta as MyTableMeta<ProjectConfig>;
+    const primary = meta.primary;
+    const row = cellContext.row.original;
+    const router = useRouter();
+    return <Button onClick={() => {
+        router.push({pathname: "/table-config", query: {projectId: row[primary]}});
+    }
+    }>{globalTranslate("table")}</Button>
+}
