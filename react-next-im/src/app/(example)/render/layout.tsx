5903e29896eaad099f84c5e0eb95e82daae2f83e
张立志 <zhanglizhi@tedu.cn>
2025-09-22 17:53:02 +0800

    add shallow =true

--- react-next-im/src/app/(example)/render/layout.tsx
+++ react-next-im/src/app/(example)/render/layout.tsx
@@ -1,34 +1,34 @@
 import "@/app/globals.css";
-import { Metadata, Viewport } from "next";
+import {Metadata, Viewport} from "next";
 import * as React from "react";
 
 export const metadata: Metadata = {
-  title: "sparrow zoo",
-  description: "welcome to sparrow zoo",
+    title: "sparrow zoo",
+    description: "welcome to sparrow zoo",
 };
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  console.log("html layout rendered");
+    console.log("html layout rendered");
 
-  return (
-    <html>
-      <body
-        className={
-          "mx-auto text-left justify-center align-middle content-center w-full "
-        }
-      >
+    return (
+        <html suppressHydrationWarning>
+        <body
+            className={
+                "mx-auto text-left justify-center align-middle content-center w-full "
+            }
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
